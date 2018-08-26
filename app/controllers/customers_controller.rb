98a2848184111f1c87ffc2b79efb83222d5e3974
Shun <shungiku99@gmail.com>
2018-08-26 17:03:15 +0900

    (3)まで終了

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,22 +1,62 @@
 class CustomersController < ApplicationController
+
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+
   def index
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def new
+    @customer = Customer.new
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    if  @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
   end
 
+
+
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if  @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect_to customers_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email,
+      :company_id,
+      :post_id
+      )
+  end  
+
+  def set_customer
+    @customer = Customer.find(params[:id])
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module SpartaCrm
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Tokyo' # タイムゾーンもついでに変更
+    config.i18n.default_locale = :ja
+
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      %Q(#{html_tag}).html_safe
+    end
   end
 end
